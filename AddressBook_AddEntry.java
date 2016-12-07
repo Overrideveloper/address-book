010509e535a13d88010b780111294b4d2eaacd3b
Banso D. Wisdom <bansowisdom@gmail.com>
2016-12-07 07:57:54 -0800

    Controller class that handles the "add entry" operation of the address book.

---
+++ AddressBook_AddEntry.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Overrideveloper
+ */
+public class AddressBook_AddEntry extends javax.swing.JFrame{
+    
+    private javax.swing.JLabel jLabel1;
+	private javax.swing.JLabel jLabel2;
+	private javax.swing.JLabel jLabel3;
+	private javax.swing.JLabel jLabel4;
+	private javax.swing.JTextField jTextField1;
+	private javax.swing.JTextField jTextField2;
+	private javax.swing.JTextField jTextField3;
+	private javax.swing.JTextField jTextField4;
+	private javax.swing.JButton jButton1;
+	private javax.swing.JButton jButton2;
+	private javax.swing.JPanel jPanel1;
+	private javax.swing.JPanel jPanel2;
+	private javax.swing.JPanel jPanel3;
+	private javax.swing.JPanel jPanel4;
+	private javax.swing.JPanel jPanel5;
+
+	public AddressBook_AddEntry(){
+		super("Add Entry");
+
+		setDefaultCloseOperation(javax.swing.JFrame.HIDE_ON_CLOSE);
+
+		setResizable(true);
+
+		jLabel1 = new javax.swing.JLabel("Contact Name: ");
+		jLabel2 = new javax.swing.JLabel("Contact Address: ");
+		jLabel3 = new javax.swing.JLabel("Contact Tel. No.: ");
+		jLabel4 = new javax.swing.JLabel("Contact Email: ");
+
+		jTextField1 = new javax.swing.JTextField(15);
+		jTextField2 = new javax.swing.JTextField(30);
+		jTextField3 = new javax.swing.JTextField(10);
+		jTextField4 = new javax.swing.JTextField(15);
+		jButton1 = new javax.swing.JButton("Add Entry");
+		jButton2 = new javax.swing.JButton("Cancel");
+
+		jPanel1 = new javax.swing.JPanel(new java.awt.GridLayout(4,1,1,5));
+
+		jPanel1.add(jLabel1);
+		jPanel1.add(jLabel2);
+		jPanel1.add(jLabel3);
+		jPanel1.add(jLabel4);
+
+		jPanel2 = new javax.swing.JPanel(new java.awt.GridLayout(4, 1));
+
+		jPanel2.add(jTextField1);
+		jPanel2.add(jTextField2);
+		jPanel2.add(jTextField3);
+		jPanel2.add(jTextField4);
+
+		jPanel3 = new javax.swing.JPanel(new java.awt.FlowLayout());
+
+		jPanel3.add(jPanel1);
+		jPanel3.add(jPanel2);
+
+		jPanel4 = new javax.swing.JPanel(new java.awt.FlowLayout());
+
+		jPanel4.add(jButton1);
+		jPanel4.add(jButton2);
+
+        }
+}
